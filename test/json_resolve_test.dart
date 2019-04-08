ffa21c956a5ef9f15b607a5c0a52aba810595dca
Khoa Pham <onmyway133@gmail.com>
2019-04-08 23:12:18 +0200

    Add test

--- test/json_resolve_test.dart
+++ test/json_resolve_test.dart
@@ -2,3 +2,68 @@
 /// json_resolve
 /// Created by Khoa Pham
 ///
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:json_resolve/json_resolve.dart';
+
+void main() {
+  final Map<String, dynamic> json = {
+    "movie": "isFun",
+    "earth": 199999,
+    "dc": [
+      {
+        "name": "Superman"
+      },
+      {
+        "name": "Flash"
+      },
+      {
+        "name": "Wonder Woman"
+      }
+    ],
+    "marvel": [
+      {
+        "name": "Thor",
+        "weapon": "Hammer",
+        "appear": [
+          {
+            "year": 2011,
+            "title": "Thor"
+          },
+          {
+            "year": 2013,
+            "title": "The Dark World"
+          },
+          {
+            "year": 2017,
+            "title": "Ragnarok"
+          }
+        ]
+      }
+    ]
+  };
+
+  test('tests accessing', () {
+    final String byProperty = resolve(json: json, path: "movie", defaultValue: "error");
+    expect(byProperty, "isFun");
+
+    final int byInt = resolve(json: json, path: "earth", defaultValue: 0);
+    expect(byInt, 199999);
+
+    final String byIndex = resolve(json: json, path: "dc.2", defaultValue: "error");
+    expect(byIndex, "Wonder Woman");
+
+    final String byIndexThenProperty = resolve(json: json, path: "marvel.appear.1.title", defaultValue: "error");
+    expect(byIndexThenProperty, "The Dark World");
+  });
+
+  test("test wrong type", () {
+    final int byInt = resolve(json: json, path: "marvel.0.name", defaultValue: 999);
+    expect(byInt, 999);
+  });
+
+  test("test not found", () {
+    final String byString = resolve(json: json, path: "marvel.0.skill", defaultValue: "error");
+    expect(byString, "error");
+  });
+}
