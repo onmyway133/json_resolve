aab945159686124e0300e9c49c806b1730d4c322
Thomas <samoht.t@hotmail.fr>
2021-03-30 19:14:39 +0200

    Migrated to Null-Safety
    - Updated SDK constraints to >=2.12.0-0 <3.0.0
    - Updated Test dev dependency to 1.16.8
    - Migrated resolve method to null safety
    - Removed unnecessary android&ios files

--- lib/json_resolve.dart
+++ lib/json_resolve.dart
@@ -3,21 +3,28 @@
 /// Created by Khoa Pham
 ///
 
-T resolve<T>({Map<String, dynamic> json, String path, T defaultValue}) {
+T resolve<T>({
+  required Map<String, dynamic>? json,
+  required String path,
+  required T defaultValue,
+}) {
   try {
+    if (json == null) {
+      return defaultValue;
+    }
     dynamic current = json;
     path.split('.').forEach((segment) {
       final maybeInt = int.tryParse(segment);
 
       if (maybeInt != null && current is List<dynamic>) {
         current = current[maybeInt];
-      } else if (current is Map<String, dynamic> ) {
+      } else if (current is Map<String, dynamic>) {
         current = current[segment];
       }
     });
 
-    return (current as T) ?? defaultValue;
-  } catch(error) {
+    return (current as T?) ?? defaultValue;
+  } catch (error) {
     print(error);
     return defaultValue;
   }
